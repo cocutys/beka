7cf60cc9e63b15456753f30fe8b10939d9983a04
cocutys <syperbeka@gmail.com>
2022-10-14 15:23:36 +0600

    removed .idea

--- home/homework3.py
+++ home/homework3.py
@@ -1,25 +1,47 @@
-class Add():
-    def __add__(self):
-        return self. + s
+class Add:
+    def __init__(self, val):
+        self.val = val
+
+    def __add__(self, other):
+        return self.val + other.val
 
 
-class Sub():
-    def __sub__(self):
-        return self. - self.
+class Sub:
+    def __init__(self, val):
+        self.val = val
+
+    def __sub__(self, other):
+        return self.val - other.val
 
 
-class Mul():
-    def __mul__(self):
-        return self. * self.
+class Mul:
+    def __init__(self, val):
+        self.val = val
+
+    def __mul__(self, other):
+        return self.val * other.val
 
 
-class Truediv():
-    def __truediv__(self):
-        return self. / self.
+class Truediv:
+    def __init__(self, val):
+        self.val = val
 
-class col(Add, Sub, Mul, Truediv):
-
-
-
+    def __truediv__(self, other):
+        return self.val / other.val
 
 
+class calc(Add, Sub, Mul, Truediv):
+    def __init__(self, val):
+        Add.__init__(self, val)
+        Sub.__init__(self, val)
+        Mul.__init__(self, val)
+        Truediv.__init__(self, val)
+
+
+num1 = calc(6)
+num2 = calc(3)
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
